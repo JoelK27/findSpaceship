c9e040b13a6f6f82e506fe59e3ad022122d68b26
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-10 18:56:35 +0200

    Step 1, Failing Test 1

--- spec/findSpaceship_spec.js
+++ spec/findSpaceship_spec.js
@@ -2,6 +2,9 @@ describe('Find Spaceship', function() {
     beforeEach(function() {
         spaceship = new Spaceship();
     });
+    
+    it('should return "Spaceship lost forever" for an empty string', function() {
+        expect(spaceship.findSpaceship('')).toEqual('Spaceship lost forever');
+    });
 });
-
-
+    
