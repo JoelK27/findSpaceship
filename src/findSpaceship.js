9ea0007825bd5adb8dc99115f38aeacd1bd87ed5
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-12 15:58:15 +0200

    Step 2, Working Test 3

--- src/findSpaceship.js
+++ src/findSpaceship.js
@@ -1,11 +1,17 @@
 function Spaceship() {}
 
 Spaceship.prototype.findSpaceship = function(map) {
-    if (map === '') {
-        return 'Spaceship lost forever';
+    var lines = map.split('\n');
+    for (var i = 0; i < lines.length; i++) {
+        for (var j = 0; j < lines[i].length; j++) {
+            if (lines[i][j] === 'X') {
+                if (i === 0 && j === 0) {
+                    return [0, 0];
+                } else {
+                    return [j + 1, i + 1];
+                }
+            }
+        }
     }
-
-    if (map === '..........\n......X...\n'){
-        return [7, 2];
-    }
+    return 'Spaceship lost forever';
 }
