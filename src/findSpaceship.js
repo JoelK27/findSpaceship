a59f7ebdb51b9d8bad0d2f27ed0de121fe1c9cc4
Joel Kakkattu <if23b095@wien-technikum.at>
2024-05-12 16:45:36 +0200

    Step 3, Refactoring Test 7

--- src/findSpaceship.js
+++ src/findSpaceship.js
@@ -3,28 +3,24 @@ function Spaceship() {}
 Spaceship.prototype.findSpaceship = function(map) {
     var lines = map.split('\n');
     var countX = (map.match(/X/g) || []).length;
+    var result = 'Spaceship lost forever.';
 
     if (!/^[\nX.]*$/.test(map)) {
-        return 'Spaceship lost forever.';
+        return result;
     }
 
     if (countX > 1) {
         return 0;
     }
 
+    outerLoop:
     for (var i = 0; i < lines.length; i++) {
         for (var j = 0; j < lines[i].length; j++) {
             if (lines[i][j] === 'X') {
-                if (i === 0 && j === 0) {
-                    return [0, 0];
-                } else {
-                    return [j + 1, i + 1];
-                }
-            }
-            else if (i === lines.length - 1 && j === lines[i].length - 1 || lines[i][j] != 'X' && lines[i][j] != '.' && lines[i][j] != '\n') {
-                return 'Spaceship lost forever.';
+                result = (i === 0 && j === 0) ? [0, 0] : [j + 1, i + 1];
+                break outerLoop;
             }
         }
     }
-    return 'Spaceship lost forever.';
+    return result;
 }
